f6a0ee1c4fb3e5372ed7f9452de7f88aef27c2ec
overho79 <overho79@users.noreply.github.com>
2017-06-19 16:35:20 -0500

    Update __init__.py
    working on adding create vacation

--- pyecobee/__init__.py
+++ pyecobee/__init__.py
@@ -225,6 +225,24 @@ class Ecobee(object):
                   " climate hold.  Refreshing tokens...")
             self.refresh_tokens()
 
+    def create_vacation(self, index, vacation):
+        ''' create a vacation with name vacation - need other details too '''
+        url = 'https://api.ecobee.com/1/thermostat'
+        header = {'Content-Type': 'application/json;charset=UTF-8',
+                  'Authorization': 'Bearer ' + self.access_token}
+        params = {'format': 'json'}
+        body = ('{"functions":[{"type":"createVacation","params":{"name":"'
+                + vacation + '"}}],'
+                '"selection":{"selectionType":"registered","selectionMatch":"'
+                '"}}')
+        request = requests.post(url, headers=header, params=params, data=body)
+        if request.status_code == requests.codes.ok:
+            return request
+        else:
+            logger.warn("Error connecting to Ecobee while attempting to create"
+                  " a vacation.  Refreshing tokens...")
+            self.refresh_tokens()
+            
     def delete_vacation(self, index, vacation):
         ''' Delete the vacation with name vacation '''
         url = 'https://api.ecobee.com/1/thermostat'
